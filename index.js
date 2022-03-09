27a1e564964ad2894f2cf827b8e00ee092319fa8
BhagyeshJoshi04 <joshibhagyeshb@gmail.com>
2022-03-09 22:52:46 +0530

    js started

---
+++ index.js
@@ -0,0 +1,41 @@
+// starting js
+// var let const
+// variable declaration 
+var a; //int string float dont need 
+// variable initialization
+a=10;
+console.log(typeof a);
+console.log(a);
+
+a="hello"; //re initialize of variable a
+console.log(a);
+// typeof operator -> it tells us the type of data stored in a variable
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+
+// // number
+// var num=10;
+// console.log(num);
+// var float=2.4;
+// console.log(float);
+
+// //boolean
+// var t=true;
+// var f=false;
+// console.log(t);
+// console.log(f);
+
+// //string 
+// // ' '," ",` `
+// // single quotes,double quotes,backtick
+// var str='a';
+// console.log(typeof str);
+// str="How you doin ?"
+// console.log(str);
